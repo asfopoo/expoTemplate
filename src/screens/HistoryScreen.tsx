cff2c68c8ef53abc04dff1c12f561ba888d74441
Ed Nardo <ednardo@Eds-MacBook-Pro-2.local>
2023-11-05 21:05:09 -0500

    no history

--- src/screens/HistoryScreen.tsx
+++ src/screens/HistoryScreen.tsx
@@ -1,8 +1,9 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { useLayoutEffect } from 'react';
-import { Text, SafeAreaView } from 'react-native';
+import { Image, SafeAreaView, StyleSheet, View, Text } from 'react-native';
 
 import { RootStackParamList } from '../navigation/types';
+import { TEXT_SIZES } from '../theme/layout';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'History Tab'>;
 
@@ -14,10 +15,60 @@ export default function HistoryScreen({ navigation }: Props) {
   }, [navigation]);
 
   return (
-    <SafeAreaView
-      style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
-    >
-      <Text>History screen</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image
+          source={require('../assets/hand_scan.png')}
+          resizeMode="stretch"
+          style={styles.image}
+        />
+      </View>
+      <View style={styles.textContainer}>
+        <View style={styles.titleContainer}>
+          <Text style={styles.largeText}>You dont have any scans today</Text>
+        </View>
+        <Text style={styles.text}>
+          Scan an ID to get started. Once you scan an ID, you will be able to
+          view the history here.
+        </Text>
+      </View>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    marginTop: '10%',
+  },
+  imageContainer: {
+    width: '80%',
+    height: '50%',
+    paddingLeft: '10%',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  image: {
+    width: '100%',
+    height: '80%',
+  },
+  largeText: {
+    fontSize: TEXT_SIZES.TEXT22,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  textContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '80%',
+    marginBottom: 40,
+  },
+  text: {
+    textAlign: 'center',
+  },
+  titleContainer: {
+    marginBottom: 20,
+  },
+});
